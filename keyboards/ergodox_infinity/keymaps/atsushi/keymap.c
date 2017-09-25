1228446409a264234d53001f0d52849876bb3fd2
Atsushi Yonenaga <a-yonenaga@r-learning.co.jp>
2017-09-25 12:57:50 +0900

    replaced HENK/MHEN with LANG2/LANG1

--- keyboards/ergodox_infinity/keymaps/atsushi/keymap.c
+++ keyboards/ergodox_infinity/keymaps/atsushi/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TT(MDIA),
         GUI_T(KC_ESC),  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   TT(SYMB),
-        LT(SYMB,KC_GRV),KC_QUOT,      KC_LALT,KC_LEFT,KC_MHEN,
+        LT(SYMB,KC_GRV),KC_QUOT,      KC_LALT,KC_LEFT,KC_LANG2,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_DELT,KC_END,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TT(MDIA),    KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,    KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
              TT(SYMB)    ,KC_N,   KC_M,    KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_HENK, KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
+                                  KC_LANG1,KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,KC_BSPC, KC_ENT
