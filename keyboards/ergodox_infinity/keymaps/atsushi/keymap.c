b0276d7c8d35d9878c9b58a6fe643331331b20ee
Atsushi Yonenaga <a-yonenaga@r-learning.co.jp>
2017-10-27 19:37:12 +0900

    左端のL1をCtrlに

--- keyboards/ergodox_infinity/keymaps/atsushi/keymap.c
+++ keyboards/ergodox_infinity/keymaps/atsushi/keymap.c
@@ -25,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |Esc/Cmd |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2| '/Cmd  |
  * |--------+------+------+------+------+------|  L1  |           |  L1  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  | Alt  |  Cmd | F13  |                                       | F14  | Cmd  |   [  |   ]  | ~L1  |
+ *   | Ctrl |Grv/L1| Alt  |  Cmd | F13  |                                       | F14  | Cmd  |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -41,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   ALL_T(KC_NO),
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TT(MDIA),
-        GUI_T(KC_ESC),  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   TT(SYMB),
-        LT(SYMB,KC_GRV),KC_QUOT,      KC_LALT,KC_LGUI,KC_F13,
-                                              ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
-                                               KC_SPC,KC_DELT,KC_END,
+        KC_EQL,         KC_1,            KC_2,    KC_3,    KC_4,   KC_5,   ALL_T(KC_NO),
+        KC_TAB,         KC_Q,            KC_W,    KC_E,    KC_R,   KC_T,   TT(MDIA),
+        GUI_T(KC_ESC),  KC_A,  KC_S,     KC_D,    KC_F,    KC_G,
+        KC_LSFT,        KC_Z,            KC_X,    KC_C,    KC_V,   KC_B,   TT(SYMB),
+        KC_LCTRL,       LT(SYMB,KC_GRV), KC_LALT, KC_LGUI, KC_F13,
+                                                           ALT_T(KC_APP),  KC_LGUI,
+                                                                           KC_HOME,
+                                                           KC_SPC,KC_DELT, KC_END,
         // right hand
              MEH_T(KC_NO),KC_6,   KC_7,    KC_8,   KC_9,   KC_0,             KC_MINS,
              TT(MDIA),    KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,             KC_BSLS,
